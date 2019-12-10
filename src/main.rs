bca9b4c54dde57fcea5c7e771d0d148a0284378f
Hubert Lin <linhub15@gmail.com>
2019-12-09 23:07:29 -0700

    play one song, pause, play next song

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
 use std::fs::File;
 use std::io::BufReader;
-
-const FILE: &str = "clear-as-water.mp3";
+use std::{thread, time};
 
 fn main() {
+    let song: File = File::open("clear-as-water.mp3").unwrap();
+    let song_2: File = File::open("kaze-no-toorimichi.mp3").unwrap();
+
+    let sink = play(song);
+
+    let duration = time::Duration::from_millis(3000);
+    thread::sleep(duration);
+
+    sink.pause();
+
+    let sink = play(song_2);
+    sink.sleep_until_end();
+}
+
+fn play(song: File) -> rodio::Sink {
     let device = rodio::default_output_device().unwrap();
     let sink = rodio::Sink::new(&device);
-
-    let file = File::open(FILE).unwrap();
-    sink.append(rodio::Decoder::new(BufReader::new(file)).unwrap());
-
-    sink.sleep_until_end();
+    let decoder = rodio::Decoder::new(BufReader::new(song)).unwrap();
+    sink.append(decoder);
+    sink
 }
